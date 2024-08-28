0e2ff9ab12831e757d78b41c4b38072edf6879b5
josue Avuh <jkomi2003@gmail.com>
2024-08-28 13:24:58 +0100

    commit

--- server/index.js
+++ server/index.js
@@ -4,16 +4,18 @@ import cors from 'cors';
 import connectDB from './mongodb/connect.js';
 
 import postRoutes from './routes/postRoutes.js';
-import dalleRoutes from './routes/dalleRoutes.js'
+import geminiRoutes from './routes/geminiRoutes.js'
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
 app.use(express.json({limit: '50mb'}));
 
 app.use('/api/v1/post', postRoutes);
-app.use('/api/v1/dalle', dalleRoutes);
+app.use('/api/v1/gemini', geminiRoutes);
 
 app.get('/', async (req, res) => {
   res.status(200).json({
