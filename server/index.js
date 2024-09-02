1b30d1a1f356808a0a2eccf0214050320a78c5e2
josue Avuh <jkomi2003@gmail.com>
2024-09-02 12:56:42 +0100

    cors changes

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: ['http://localhost:5173', 'https://imagen-stable.vercel.app/'],
 }));
 app.use(express.json({limit: '50mb'}));
 
