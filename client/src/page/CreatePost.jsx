af47c27a8792ff2d3cbb6326348069c23f1de73f
josue Avuh <jkomi2003@gmail.com>
2024-10-07 16:16:55 +0100

    backend url change

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -30,7 +30,7 @@ const CreatePost = () => {
       try {
         setGeneratingImg(true);
         setWaitingForImage(false);
-        const response = await fetch('https://imagen-5bai.onrender.com/api/v1/fal/', {
+        const response = await fetch('https://imagen-x8do.vercel.app/api/v1/fal/', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -60,7 +60,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('https://imagen-5bai.onrender.com/api/v1/post', {
+        const response = await fetch('https://imagen-x8do.vercel.app/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://imagen-5bai.onrender.com/api/v1/post', {
+      const response = await fetch('https://imagen-x8do.vercel.app/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
